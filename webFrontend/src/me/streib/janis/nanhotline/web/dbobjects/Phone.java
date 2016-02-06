ee08b3aaa26a544678fc38d542ed9b62a4a5408b
Janis Streib <janis@dogcraft.de>
2016-02-06 15:03:06 +0100

    ADD: Phone by id

--- webFrontend/src/me/streib/janis/nanhotline/web/dbobjects/Phone.java
+++ webFrontend/src/me/streib/janis/nanhotline/web/dbobjects/Phone.java
@@ -1,14 +1,26 @@
 package me.streib.janis.nanhotline.web.dbobjects;
 
-import me.streib.janis.nanhotline.web.DatabaseConnection;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import me.streib.janis.nanhotline.web.DatabaseConnection;
+
 public class Phone {
     private int id;
     private String sipUri;
+
+    public static Phone getById(int id) throws SQLException {
+        PreparedStatement prep = DatabaseConnection.getInstance().prepare(
+                "SELECT * FROM supporter_phones WHERE id=?");
+        prep.setInt(1, id);
+        try (ResultSet res = prep.executeQuery()) {
+            if (res.next()) {
+                return new Phone(res);
+            }
+        }
+        return null;
+    }
 
     public Phone(ResultSet res) throws SQLException {
         id = res.getInt("id");
@@ -17,9 +29,10 @@ public class Phone {
 
     public Phone(String sipUri, Supporter supporter) throws SQLException {
         this.sipUri = sipUri;
-        PreparedStatement prep = DatabaseConnection.getInstance().prepare(
-                "INSERT INTO supporter_phones (sip_uri, supporter) VALUES (?, ?)"
-        );
+        PreparedStatement prep = DatabaseConnection
+                .getInstance()
+                .prepare(
+                        "INSERT INTO supporter_phones (sip_uri, supporter) VALUES (?, ?)");
         prep.setString(1, sipUri);
         prep.setInt(2, supporter.getId());
         prep.execute();
@@ -29,8 +42,7 @@ public class Phone {
 
     public static void deleteById(int id, int supporter_id) throws SQLException {
         PreparedStatement prep = DatabaseConnection.getInstance().prepare(
-                "DELETE FROM supporter_phones WHERE id=? AND supporter=?"
-        );
+                "DELETE FROM supporter_phones WHERE id=? AND supporter=?");
         prep.setInt(1, id);
         prep.setInt(2, supporter_id);
         prep.execute();
